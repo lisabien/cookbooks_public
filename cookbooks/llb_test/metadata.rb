9e6efdd5113adf760ac0a519aa1e310779c82e3c
Lisa Bien <lisabien@yahoo.com>
2012-02-27 17:17:47 -0800

    Try adding new llb_test cookbook to my cloned repository

---
+++ cookbooks/llb_test/metadata.rb
@@ -0,0 +1,16 @@
+maintainer       "RightScale, Inc."
+maintainer_email "lisa.bien@rightscale.com"
+license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
+description      "Learning to use Chef/Cookbooks/Recipes"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
+
+recipe "llb_test::helloworld", "My first recipe, prints Hello World to the RightScale dashboard"
+recipe "llb_test::hello", "My second recipe, prints Hello NAME to the RightScale dashboard"
+# add here the new recipe
+attribute "name",
+:display_name => "Test value",
+:description => "Test specifying Input via Chef attribute",
+:required => true,
+:recipes => [ "llb_test::hello" ]
+# add here the new attribute
---
+++ cookbooks/llb_test/recipes/hello.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: llb_test
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+# This recipe prints a message
+rs_utils_marker :begin
+  log  "In hello.rb!!!"
+  puts "Hello World, #{node[:name]}"
+rs_utils_marker :end
